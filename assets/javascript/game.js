6d49e0139054bc7f5751f17eceb6eeff1d02bc8d
Bonnie Acuna <bonnieacuna@Bonnies-MacBook-Pro.local>
2018-11-03 12:05:25 -0700

    finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,53 +6,53 @@ var lossNumber = 0;
 var lettersGuessed = [];
 var guessLetter = null;
 var winningLetter = letters[Math.floor(Math.random() * letters.length)];
-//We need code to update the DOM
-console.log(winningLetter);
 
-//Loop through the letters
-for (var i = 0; i < letters.length; i++) {
-
-};
-
-// function myFunction() {
-//     document.getElementById("button").innerHTML = " ";
-  
-document.onkeyup = function(event) {
-    var guessLetter = event.key.toLowerCase(); 
-
-
-    if (winningLetter === guessLetter) {
-        //This means we just won, does the user want to play agin, 
-        //if so we need to reset the state back to the beginning of the game
-        winsNumber++;
-        alert("You Won!");
-        var winsEl = document.getElementById('wins');
-        winsEl.textContent = "Wins: " + winsNumber;
-        // var buttonContainer = document.getElementById("button");
-        // buttonContainer.innerHTML = ("<button onclick='myFunction()'>Restart Game</button>");
-
-        //Now we need to  ask the user if they want to play again>
-    }
+//Start the Game
+document.onkeyup = function (event) {
+    var guessLetter = event.key.toLowerCase();
     
-
-    else {
-        //subtract a guess
-        //check if the user lost
-        (guessedNumber === 0) 
-        lossNumber++;
-        alert("You Lose");
-        var lossEl = document.getElementById('loss');
-        lossEl.textContent = "Loss: " + lossNumber;
-        // var buttonContainer = document.getElementById("button");
-        // buttonContainer.innerHTML = ("<button onclick='myFunction()'>Restart Game</button>");
-
-  
+//Resets the Game
+    var restartGame = function () {
+        guessedNumber = 5;
+        lettersGuessed = [];
+        winningLetter = letters[Math.floor(Math.random() * letters.length)];
     }
 
+//Picks the letters and makes sure it doesn't repeat or use non letters.
+    if (letters.indexOf(guessLetter) > -1 && lettersGuessed.indexOf(guessLetter) ===-1) {
 
+//If the winning letter is picked       
+        if (winningLetter === guessLetter) {
+            winsNumber++;
+            alert("You Won!");
+            var winsEl = document.getElementById('wins');
+            winsEl.textContent = "Wins: " + winsNumber;
+            lettersGuessed = [];
+            restartGame();
+
+//If the user loses            
+        } else if (guessedNumber === 0) {
+            lossNumber++;
+            alert("You Lose");
+            var lossEl = document.getElementById('loss');
+            lossEl.textContent = "Loss: " + lossNumber;
+            lettersGuessed = [];
+            restartGame();
+
+//Shows the number of guesses           
+        } else {
+            guessedNumber--;
+            lettersGuessed.push(guessLetter)
+            var numberEl = document.getElementById('guess-number');
+            numberEl.textContent = "Numbers Guessed: " + guessedNumber;
+            
+        }
+       
+
+//Shows the letters guessed       
+        document.getElementById("guesses").textContent = ("Letter Guesses: " + lettersGuessed.join(" , "));
+    }
 }
-    ("guesses").textContent = ("Letter Guesses: " + letters.join(" , "));
-    ("guess-number").textContent = ("Number of Guesses: " + guessedNumber);
 
 
 
@@ -98,4 +98,3 @@ document.onkeyup = function(event) {
 
 
 
- 
